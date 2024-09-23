f9d29d8af4d8307d99cad122a47c2a86deeeecbc
RubenMartins97 <rubendevgo@gmail.com>
2024-09-23 15:36:25 -0100

    feat: Add responsivity to hero section texts

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,23 +1,38 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Typography, useTheme } from "@mui/material";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
 const Hero: React.FC = () => {
   const { t } = useTranslation();
+
+  const theme = useTheme();
+
   const typographyStyle = {
     headline: {
       fontFamily: "Istok Web",
       fontStyle: "normal",
       fontWeight: 700,
       fontSize: {
-        xs: "32px ",
-        sm: "32px ",
+        xs: "22px",
+        sm: "22px",
         md: "32px ",
         lg: "36px ",
         xl: "48px",
       },
-      lineHeight: "58px",
-      textAlign: "justify",
+      lineHeight: {
+        xs: "32px",
+        sm: "32px",
+        md: "58px",
+        lg: "58px",
+        xl: "58px",
+      },
+      textAlign: {
+        xs: "center",
+        sm: "center",
+        md: "center",
+        lg: "justify",
+        xl: "justify",
+      },
       color: "#000000",
     },
     subheadline: {
@@ -25,8 +40,8 @@ const Hero: React.FC = () => {
       fontStyle: "normal",
       fontWeight: 400,
       fontSize: {
-        xs: "18px",
-        sm: "18px",
+        xs: "16px",
+        sm: "16px",
         md: "18px",
         lg: "20px",
         xl: "24px",
@@ -54,34 +69,59 @@ const Hero: React.FC = () => {
       color: "#000000",
       marginTop: "50px",
       textTransform: "none",
+      "&:hover": {
+        backgroundColor: theme.palette.secondary.main,
+        color: "#FFFFFF",
+      },
     },
   };
   return (
     <Box
       sx={{
-        height: "922px",
         display: "flex",
         flexDirection: "row",
-        justifyContent: "space-between",
+        justifyContent: {
+          xs: "center",
+          sm: "center",
+          md: "center",
+          lg: "space-between",
+          xl: "space-between",
+        },
         marginTop: "5%",
       }}
     >
       <Box
         sx={{
           width: "100%",
-          // background: {
-          //   xs: "red",
-          //   sm: "blue",
-          //   md: "green",
-          //   lg: "yellow",
-          //   xl: "violet",
-          // },
+          display: {
+            xs: "flex",
+            sm: "flex",
+            md: "flex",
+            lg: "block",
+            xl: "block",
+          },
+          justifyContent: "center",
         }}
       >
         <Box
           sx={{
-            marginLeft: "60px",
+            marginLeft: {
+              xs: "0px",
+              sm: "0px",
+              md: "0px",
+              lg: "60px",
+              xl: "60px",
+            },
             width: "75%",
+            display: {
+              xs: "flex",
+              sm: "flex",
+              md: "flex",
+              lg: "block",
+              xl: "block",
+            },
+            alignItems: "center",
+            flexDirection: "column",
           }}
         >
           <Typography sx={typographyStyle.headline}>
@@ -92,14 +132,24 @@ const Hero: React.FC = () => {
             {t("homepage.hero.subheadline")}
           </Typography>
 
-          <Button variant="contained" sx={typographyStyle.cta}>
+          <Button
+            variant="contained"
+            sx={typographyStyle.cta}
+            disableElevation={true}
+          >
             {t("homepage.hero.cta")}
           </Button>
         </Box>
       </Box>
       <Box
         sx={{
-          display: "flex",
+          display: {
+            xs: "none",
+            sm: "none",
+            md: "none",
+            lg: "flex",
+            xl: "flex",
+          },
           justifyContent: "center",
           width: "65%",
         }}
