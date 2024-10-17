a291c307372ccad1c0c8a472cda00399a574a85b
Danilson Reis <danyreys47@gmail.com>
2024-10-17 15:57:03 -0100

    chore: implement contact section on xl and lg screens

--- src/components/Contact/EmailForm.tsx
+++ src/components/Contact/EmailForm.tsx
@@ -5,30 +5,94 @@ const EmailForm = () => {
 
   return (
     <Box
-      // component="form"
-      // autoComplete="off"
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "1rem",
+        background: "linear-gradient(180deg, #FE9E00 0%, #0C666E 100%)",
+        padding: "8rem 0",
       }}
+      display="flex"
+      justifyContent="center"
     >
-      <Typography>Entre em contato connosco</Typography>
-      <TextField required id="name" placeholder="Insira o seu nome" />
-      <TextField required id="email" placeholder="Insira um e-mail válido" />
-      <TextField required id="message" placeholder="Insira a sua mensagem" />
-      <Button
-        variant="contained"
+      <Box
+        component="form"
+        autoComplete="off"
         sx={{
-          color: "#ffffff",
-          background: theme.palette.secondary.main,
-          "&:hover": {
-            backgroundColor: theme.palette.secondary.dark,
-          },
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "1.5rem",
+          width: { xs: "50%", xl: "45%" },
         }}
       >
-        Enviar
-      </Button>
+        <Typography variant="h3">Garanta a Sua Reserva por E-mail</Typography>
+
+        <TextField
+          required
+          id="name"
+          placeholder="Insira o seu nome*"
+          size="small"
+          sx={{
+            width: "100%",
+            "& .MuiInputBase-root": {
+              borderRadius: "4px",
+            },
+            input: {
+              background: "#fff",
+              borderRadius: "4px",
+            },
+          }}
+        />
+        <TextField
+          required
+          id="email"
+          placeholder="Insira um e-mail válido*"
+          size="small"
+          sx={{
+            width: "100%",
+            "& .MuiInputBase-root": {
+              borderRadius: "4px",
+            },
+            input: {
+              background: "#fff",
+              borderRadius: "4px",
+            },
+          }}
+        />
+        <TextField
+          required
+          id="message"
+          placeholder="Insira a sua mensagem*"
+          size="small"
+          multiline
+          rows="5"
+          sx={{
+            width: "100%",
+            "& .MuiInputBase-root": {
+              padding: "0",
+              borderRadius: "4px",
+            },
+            textarea: {
+              padding: "8.5px 14px",
+              background: "#fff",
+              borderRadius: "4px",
+            },
+          }}
+        />
+        <Button
+          variant="contained"
+          sx={{
+            color: "#ffffff",
+            background: theme.palette.secondary.main,
+            "&:hover": {
+              backgroundColor: theme.palette.secondary.dark,
+            },
+            width: "40%",
+            alignSelf: "start",
+            marginTop: "1rem",
+          }}
+        >
+          Enviar
+        </Button>
+      </Box>
     </Box>
   );
 };
