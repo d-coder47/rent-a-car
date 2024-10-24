06a4ca57ef0cab8b6fa39dcc59e0989317f9754f
rubenmartins21 <rubenmartins463@gmail.com>
2024-10-24 21:46:45 -0100

    feat: Add responsivity to contact section on home page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -6,15 +6,38 @@ const Contact = () => {
   const { t } = useTranslation();
 
   return (
-    <Grid2 id="contactus" display="flex" flexWrap="wrap">
+    <Grid2
+      id="contactus"
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
       <Box display="flex" justifyContent="center" width={{ xs: "100%" }}>
         <Typography variant="h2">{t("homepage.contact.headline")}</Typography>
       </Box>
-      <Box display="flex" p="4rem 0 0 0" width={{ xs: "100%" }}>
-        <Box width={{ xs: "50%" }}>
+      <Box
+        p="4rem 0 0 0"
+        sx={{
+          display: "flex",
+          flexDirection: {
+            xs: "column",
+            sm: "column",
+            md: "row",
+            lg: "row",
+            xl: "row",
+          },
+          width: "100%",
+        }}
+      >
+        <Box
+          width={{ xs: "100%", sm: "100%", md: "50%", lg: "50%", xl: "50%" }}
+        >
           <EmailForm />
         </Box>
-        <Box width={{ xs: "50%" }}>
+        <Box
+          width={{ xs: "100%", sm: "100%", md: "50%", lg: "50%", xl: "50%" }}
+        >
           <Box
             component="iframe"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3817.805222246798!2d-24.986853724089297!3d16.885527117067202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f1fa03d36fcc5%3A0x62a0dfffe32eef8b!2sV%26H%20INVESTIMENTOS%20-%20RENT%20A%20CAR%2F%20IMPORTA%C3%87%C3%83O%20DE%20VIATURAS%20%26%20PE%C3%87AS!5e0!3m2!1spt-PT!2scv!4v1729182210866!5m2!1spt-PT!2scv"
