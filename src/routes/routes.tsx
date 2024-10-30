f45955fb25ac3442ec9119f428bc89a3369e3f7a
Danilson Reis <danyreys47@gmail.com>
2024-10-30 12:53:53 -0100

    feat: implement single car page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Cars from "../pages/Cars";
+import Car from "../pages/Car";
 
 const AppRoutes: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cars" element={<Cars />} />
+        <Route path="/car" element={<Car />} />
       </Routes>
     </Router>
   );
