950460c4060926141df5092e4a630265a41f9943
Danilson Reis <danilson.reis@devgo.org>
2024-12-12 18:47:04 -0100

    wip: implement single car page

--- src/pages/Car.tsx
+++ src/pages/Car.tsx
@@ -7,33 +7,42 @@ import {
   ListItemText,
   Typography,
 } from "@mui/material";
+import { PortableText } from "@portabletext/react";
 import { useTranslation } from "react-i18next";
 import Layout from "../components/Layout/Layout";
 import { HEADER_HEIGHT, PRE_HEADER_HEIGHT } from "../constants";
+import { useCar } from "../context/CarContext";
+import { useParams } from "react-router-dom";
+import { formatPrice } from "../utils";
+import CarGallery from "../components/CarGallery/CarGallery";
 
 const Car = () => {
-  const { t } = useTranslation();
+  const { slug } = useParams();
 
-  const info = {
-    "Brand-Name": "Subaru",
-    Model: "Ascent",
-    Year: 2020,
-    Seats: 7,
-    Fuel: "Gasoline",
-    Mileage: "20-21 MPG (city), 26-27 MPG (highway)",
-    Motor: "2.4L Turbocharged Boxer 4-cylinder engine",
-    "Colors-Available": "White",
-    Description:
-      "O Subaru Ascent 2020 é um SUV espaçoso e versátil, ideal para famílias que buscam conforto e segurança. Com capacidade para até oito passageiros, ele conta com tração integral (AWD) de série, proporcionando estabilidade em diferentes terrenos. Seu motor turbo 2.4L entrega uma performance eficiente, combinada com uma economia de combustível competitiva para a categoria. Além disso, o Ascent oferece tecnologias avançadas de assistência ao motorista, como o sistema EyeSight, para maior segurança e tranquilidade na condução.",
-  };
+  const { t } = useTranslation();
+  const { cars } = useCar();
+
+  const carData = cars?.find((car) => car.slug.current === slug);
+
+  const carPrice =
+    carData && carData?.priceToRent?.amount > 0
+      ? formatPrice(carData?.priceToRent.amount, carData?.priceToRent.currency)
+      : formatPrice(
+          carData?.priceToSell.amount || 0,
+          carData?.priceToSell.currency || ""
+        );
+
+  if (!carData) {
+    return <Box>DISPLAY ERROR PAGE</Box>;
+  }
 
   return (
     <Layout>
-      <Grid2
+      {/* <Grid2
         container
         display="flex"
         flexWrap="wrap"
-        height={`calc(100vh - ${PRE_HEADER_HEIGHT + HEADER_HEIGHT})`}
+        // height={`calc(100vh - ${PRE_HEADER_HEIGHT + HEADER_HEIGHT})`}
         sx={{
           backgroundColor: "#f4f4f4",
         }}
@@ -64,73 +73,80 @@ const Car = () => {
           flexDirection="column"
           alignItems="center"
           gap="2rem"
-          marginTop={{ xs: "1rem", lg: "4rem" }}
+          marginTop={{
+            xs: "1rem",
+            lg: `calc(${PRE_HEADER_HEIGHT + HEADER_HEIGHT})`,
+          }}
           flexBasis={{ xs: "100%", md: "100%", lg: "25%", xl: "20%" }}
         >
           <Typography variant="h2">
-            {info["Brand-Name"] + " " + info.Model}
+            {carData.name + " " + carData.model}
           </Typography>
-          <Typography
-            variant="body2"
-            sx={{
-              textAlign: "justify",
-              padding: { xs: "0 4rem", lg: "0 1.5rem" },
+          <PortableText
+            value={carData.description}
+            components={{
+              block: {
+                normal: ({ children }) => (
+                  <body style={{ fontFamily: "'Istok Web', sans-serif" }}>
+                    {children}
+                  </body>
+                ),
+                h1: ({ children }) => (
+                  <h1
+                    style={{
+                      fontFamily: "'Istok Web', sans-serif",
+                      fontWeight: 700,
+                    }}
+                  >
+                    {children}
+                  </h1>
+                ),
+                h4: ({ children }) => (
+                  <h4
+                    style={{
+                      fontFamily: "'Istok Web', sans-serif",
+                      fontWeight: 700,
+                    }}
+                  >
+                    {children}
+                  </h4>
+                ),
+              },
+              list: {
+                bullet: ({ children }) => (
+                  <ul
+                    style={{
+                      fontFamily: "'Istok Web', sans-serif",
+                    }}
+                  >
+                    {children}
+                  </ul>
+                ),
+                number: ({ children }) => (
+                  <ol
+                    style={{
+                      fontFamily: "'Istok Web', sans-serif",
+                    }}
+                  >
+                    {children}
+                  </ol>
+                ),
+
+                // Ex. 2: rendering custom lists
+                checkmarks: ({ children }) => (
+                  <ol
+                    style={{
+                      fontFamily: "'Istok Web', sans-serif",
+                      fontWeight: 700,
+                    }}
+                  >
+                    {children}
+                  </ol>
+                ),
+              },
             }}
-          >
-            {info.Description}
-          </Typography>
-          <Typography
-            variant="h3"
-            sx={{
-              alignSelf: { xs: "start", lg: "center" },
-              marginLeft: { xs: "4rem", lg: "1.5rem" },
-            }}
-          >
-            {t("single-car.details")}
-          </Typography>
-          <List
-            sx={{
-              marginTop: "-2rem",
-              alignSelf: { xs: "start", lg: "center" },
-              marginLeft: { xs: "4rem", lg: "1.5rem" },
-            }}
-          >
-            <ListItem sx={{ padding: "0 1rem" }}>
-              <ListItemText
-                primary={
-                  <Typography variant="body2">• Subaru Ascent 2020</Typography>
-                }
-              />
-            </ListItem>
-            <ListItem sx={{ padding: "0 1rem" }}>
-              <ListItemText
-                primary={
-                  <Typography variant="body2">
-                    • Motor 2360c.c/ cm3, 4 cilindros
-                  </Typography>
-                }
-              />
-            </ListItem>
-            <ListItem sx={{ padding: "0 1rem" }}>
-              <ListItemText
-                primary={<Typography variant="body2">• 7 assentos</Typography>}
-              />
-            </ListItem>
-            <ListItem sx={{ padding: "0 1rem" }}>
-              <ListItemText
-                primary={
-                  <Typography variant="body2">
-                    • Transmissão Automática
-                  </Typography>
-                }
-              />
-            </ListItem>
-            <ListItem sx={{ padding: "0 1rem" }}>
-              <ListItemText
-                primary={<Typography variant="body2">• Gasolina</Typography>}
-              />
-            </ListItem>
-          </List>
+          />
+
           <Box
             className="single-car-price-box"
             display="flex"
@@ -142,13 +158,148 @@ const Car = () => {
               padding: ".5rem 1rem",
             }}
           >
-            <Typography variant="h3">€19,100</Typography>
+            <Typography variant="h3">{carPrice}</Typography>
             <Button variant="contained" sx={{ textTransform: "unset" }}>
               <Typography variant="body1">{t("single-car.rent")}</Typography>
             </Button>
           </Box>
         </Box>
-      </Grid2>
+      </Grid2> */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          background: "#f4f4f4",
+          marginTop:
+            +HEADER_HEIGHT.replace("px", "") +
+            +PRE_HEADER_HEIGHT.replace("px", "") +
+            "px",
+        }}
+      >
+        <Box
+          id="car-3d"
+          flexBasis={{ xs: "100%", lg: "75%", xl: "80%" }}
+          display="flex"
+          justifyContent="center"
+          height="auto"
+        >
+          <Box
+            component="iframe"
+            src="https://reisdanilson47.sirv.com/Subaru/Subaru.spin"
+            title="Car in 3D"
+            allowFullScreen
+            sx={{
+              width: "100%",
+              height: "auto",
+              border: "none",
+              overflow: "hidden",
+            }}
+          />
+        </Box>
+        <Box
+          id="car-info"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          gap="2rem"
+          marginTop={{
+            xs: "1rem",
+            lg: `calc(${PRE_HEADER_HEIGHT + HEADER_HEIGHT})`,
+          }}
+          flexBasis={{ xs: "100%", md: "100%", lg: "25%", xl: "20%" }}
+        >
+          <Typography variant="h2">
+            {carData.name + " " + carData.model}
+          </Typography>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            gap="1rem"
+          >
+            <PortableText
+              value={carData.description}
+              components={{
+                block: {
+                  normal: ({ children }) => (
+                    <body style={{ fontFamily: "'Istok Web', sans-serif" }}>
+                      {children}
+                    </body>
+                  ),
+                  h1: ({ children }) => (
+                    <h1
+                      style={{
+                        fontFamily: "'Istok Web', sans-serif",
+                        fontWeight: 700,
+                      }}
+                    >
+                      {children}
+                    </h1>
+                  ),
+                  h4: ({ children }) => (
+                    <h4
+                      style={{
+                        fontFamily: "'Istok Web', sans-serif",
+                        fontWeight: 700,
+                      }}
+                    >
+                      {children}
+                    </h4>
+                  ),
+                },
+                list: {
+                  bullet: ({ children }) => (
+                    <ul
+                      style={{
+                        fontFamily: "'Istok Web', sans-serif",
+                      }}
+                    >
+                      {children}
+                    </ul>
+                  ),
+                  number: ({ children }) => (
+                    <ol
+                      style={{
+                        fontFamily: "'Istok Web', sans-serif",
+                      }}
+                    >
+                      {children}
+                    </ol>
+                  ),
+
+                  // Ex. 2: rendering custom lists
+                  checkmarks: ({ children }) => (
+                    <ol
+                      style={{
+                        fontFamily: "'Istok Web', sans-serif",
+                        fontWeight: 700,
+                      }}
+                    >
+                      {children}
+                    </ol>
+                  ),
+                },
+              }}
+            />
+          </Box>
+          <Box
+            className="single-car-price-box"
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            margin="auto 1rem 1rem 1rem"
+            sx={{
+              boxShadow: 5,
+              padding: ".5rem 1rem",
+            }}
+          >
+            <Typography variant="h3">{carPrice}</Typography>
+            <Button variant="contained" sx={{ textTransform: "unset" }}>
+              <Typography variant="body1">{t("single-car.rent")}</Typography>
+            </Button>
+          </Box>
+        </Box>
+      </Box>
     </Layout>
   );
 };
