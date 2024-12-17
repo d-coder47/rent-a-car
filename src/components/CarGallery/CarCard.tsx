063f3c5ee3e8ef5920aa49a32fabe370e55651ed
rubenmartins21 <rubenmartins463@gmail.com>
2024-12-17 22:16:54 -0100

    feat:  Add redirect to car single page from cars gallery card

--- src/components/CarGallery/CarCard.tsx
+++ src/components/CarGallery/CarCard.tsx
@@ -16,7 +16,7 @@ import { ICarCard } from "../../interfaces";
 import { LocalGasStation, People } from "@mui/icons-material";
 import GearShift from "../../assets/gearshift.svg";
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { urlFor } from "../../lib/client";
 import { formatPrice } from "../../utils";
 
@@ -37,138 +37,146 @@ const CarCard: React.FC<ICarCard> = ({ car, type }) => {
       sx={{
         "&:hover": {
           boxShadow: 20,
+          cursor: "pointer",
         },
       }}
     >
-      <CardHeader
-        title={car.name + " " + car.model + " " + car.year}
-        titleTypographyProps={{ variant: "h5" }}
-        subheaderTypographyProps={{ variant: "body2" }}
-      />
-      <CardMedia
-        component="img"
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          objectFit: "contain",
-          aspectRatio: "3/2",
-        }}
-        image={urlFor(car.image).url()}
-        alt={car.name}
-      />
-      <CardContent
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          marginTop: type === "reservation" ? "25px" : "60px",
-        }}
+      <Link
+        to={`/car/${car.slug.current}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: "none", color: "inherit" }}
       >
-        <Stack direction="row" alignItems="center" gap={0.5}>
-          <LocalGasStation fontSize="small" />
-          <Typography
-            sx={{ color: "text.secondary", fontSize: 14, marginTop: "2px" }}
-          >
-            {t(`homepage.cars.${car.fuel}`)}
-          </Typography>
-        </Stack>
-
-        <Stack direction="row" alignItems="center" gap={0.5}>
-          <Icon>
-            <img src={GearShift} height={20} width={20} />
-          </Icon>
-          <Typography
-            sx={{ color: "text.secondary", fontSize: 14, marginTop: "3px" }}
-          >
-            {t(`homepage.cars.${car.transmission}`)}
-          </Typography>
-        </Stack>
-
-        <Stack direction="row" alignItems="center" gap={0.5}>
-          <People fontSize="small" />
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-around",
-            }}
-          >
-            <Typography
-              variant="body2"
-              sx={{ color: "text.secondary", fontSize: 14, marginTop: "2px" }}
-            >
-              {car.seats}
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{
-                color: "text.secondary",
-                fontSize: 14,
-                marginTop: "2px",
-                marginLeft: "3px",
-              }}
-            >
-              {t("homepage.cars.people")}
-            </Typography>
-          </Box>
-        </Stack>
-      </CardContent>
-      <CardActions>
-        <Box
+        <CardHeader
+          title={car.name + " " + car.model + " " + car.year}
+          titleTypographyProps={{ variant: "h5" }}
+          subheaderTypographyProps={{ variant: "body2" }}
+        />
+        <CardMedia
+          component="img"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            objectFit: "contain",
+            aspectRatio: "3/2",
+          }}
+          image={urlFor(car.image).url()}
+          alt={car.name}
+        />
+        <CardContent
           sx={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
-            width: "100%",
+            marginTop: type === "reservation" ? "25px" : "60px",
           }}
         >
+          <Stack direction="row" alignItems="center" gap={0.5}>
+            <LocalGasStation fontSize="small" />
+            <Typography
+              sx={{ color: "text.secondary", fontSize: 14, marginTop: "2px" }}
+            >
+              {t(`homepage.cars.${car.fuel}`)}
+            </Typography>
+          </Stack>
+
+          <Stack direction="row" alignItems="center" gap={0.5}>
+            <Icon>
+              <img src={GearShift} height={20} width={20} />
+            </Icon>
+            <Typography
+              sx={{ color: "text.secondary", fontSize: 14, marginTop: "3px" }}
+            >
+              {t(`homepage.cars.${car.transmission}`)}
+            </Typography>
+          </Stack>
+
+          <Stack direction="row" alignItems="center" gap={0.5}>
+            <People fontSize="small" />
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-around",
+              }}
+            >
+              <Typography
+                variant="body2"
+                sx={{ color: "text.secondary", fontSize: 14, marginTop: "2px" }}
+              >
+                {car.seats}
+              </Typography>
+              <Typography
+                variant="body2"
+                sx={{
+                  color: "text.secondary",
+                  fontSize: 14,
+                  marginTop: "2px",
+                  marginLeft: "3px",
+                }}
+              >
+                {t("homepage.cars.people")}
+              </Typography>
+            </Box>
+          </Stack>
+        </CardContent>
+        <CardActions>
           <Box
             sx={{
               display: "flex",
               flexDirection: "row",
-              justifyContent: "space-around",
+              justifyContent: "space-between",
+              width: "100%",
             }}
           >
-            <Typography
-              variant="body1"
+            <Box
               sx={{
-                fontWeight: "700",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-around",
               }}
             >
-              {price}
-            </Typography>
-            {isCarForRent ? (
-              <Typography variant="body1">
-                {" "}
-                {"/" + t("homepage.cars.day")}
+              <Typography
+                variant="body1"
+                sx={{
+                  fontWeight: "700",
+                }}
+              >
+                {price}
               </Typography>
-            ) : null}
+              {isCarForRent ? (
+                <Typography variant="body1">
+                  {" "}
+                  {"/" + t("homepage.cars.day")}
+                </Typography>
+              ) : null}
+            </Box>
+
+            <Button
+              variant="contained"
+              onClick={() => navigate(`/reservation/${car.slug.current}`)}
+              sx={{
+                height: "35px",
+                textAlign: "center",
+                color: "#000000",
+
+                textTransform: "none",
+                "&:hover": {
+                  backgroundColor: theme.palette.secondary.main,
+                  color: "#ffffff !important",
+                },
+              }}
+              disableElevation={true}
+            >
+              <Typography variant="body2">
+                {isCarForRent
+                  ? t("homepage.cars.reservation")
+                  : t("homepage.cars.buy")}
+              </Typography>
+            </Button>
           </Box>
-
-          <Button
-            variant="contained"
-            onClick={() => navigate(`/reservation/${car.slug.current}`)}
-            sx={{
-              height: "35px",
-              textAlign: "center",
-              color: "#000000",
-
-              textTransform: "none",
-              "&:hover": {
-                backgroundColor: theme.palette.secondary.main,
-                color: "#ffffff !important",
-              },
-            }}
-            disableElevation={true}
-          >
-            <Typography variant="body2">
-              {isCarForRent
-                ? t("homepage.cars.reservation")
-                : t("homepage.cars.buy")}
-            </Typography>
-          </Button>
-        </Box>
-      </CardActions>
+        </CardActions>
+      </Link>
     </Card>
   );
 };
