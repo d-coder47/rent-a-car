224ba938a4a2cb77968ceef2fcf42a99ecacd976
rubenmartins21 <rubenmartins463@gmail.com>
2024-12-17 21:09:25 -0100

    feat: Add scrollbar to the car description

--- src/pages/Car.tsx
+++ src/pages/Car.tsx
@@ -15,7 +15,6 @@ import { HEADER_HEIGHT, PRE_HEADER_HEIGHT } from "../constants";
 import { useCar } from "../context/CarContext";
 import { useParams } from "react-router-dom";
 import { formatPrice } from "../utils";
-import CarGallery from "../components/CarGallery/CarGallery";
 import { urlFor } from "../lib/client";
 
 const Car = () => {
@@ -40,133 +39,6 @@ const Car = () => {
 
   return (
     <Layout>
-      {/* <Grid2
-        container
-        display="flex"
-        flexWrap="wrap"
-        // height={`calc(100vh - ${PRE_HEADER_HEIGHT + HEADER_HEIGHT})`}
-        sx={{
-          backgroundColor: "#f4f4f4",
-        }}
-      >
-        <Box
-          id="car-3d"
-          flexBasis={{ xs: "100%", lg: "75%", xl: "80%" }}
-          display="flex"
-          justifyContent="center"
-          height="auto"
-        >
-          <Box
-            component="iframe"
-            src="https://reisdanilson47.sirv.com/Subaru/Subaru.spin"
-            title="Car in 3D"
-            allowFullScreen
-            sx={{
-              width: "100%",
-              height: "auto",
-              border: "none",
-              overflow: "hidden",
-            }}
-          />
-        </Box>
-        <Box
-          id="car-info"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          gap="2rem"
-          marginTop={{
-            xs: "1rem",
-            lg: `calc(${PRE_HEADER_HEIGHT + HEADER_HEIGHT})`,
-          }}
-          flexBasis={{ xs: "100%", md: "100%", lg: "25%", xl: "20%" }}
-        >
-          <Typography variant="h2">
-            {carData.name + " " + carData.model}
-          </Typography>
-          <PortableText
-            value={carData.description}
-            components={{
-              block: {
-                normal: ({ children }) => (
-                  <body style={{ fontFamily: "'Istok Web', sans-serif" }}>
-                    {children}
-                  </body>
-                ),
-                h1: ({ children }) => (
-                  <h1
-                    style={{
-                      fontFamily: "'Istok Web', sans-serif",
-                      fontWeight: 700,
-                    }}
-                  >
-                    {children}
-                  </h1>
-                ),
-                h4: ({ children }) => (
-                  <h4
-                    style={{
-                      fontFamily: "'Istok Web', sans-serif",
-                      fontWeight: 700,
-                    }}
-                  >
-                    {children}
-                  </h4>
-                ),
-              },
-              list: {
-                bullet: ({ children }) => (
-                  <ul
-                    style={{
-                      fontFamily: "'Istok Web', sans-serif",
-                    }}
-                  >
-                    {children}
-                  </ul>
-                ),
-                number: ({ children }) => (
-                  <ol
-                    style={{
-                      fontFamily: "'Istok Web', sans-serif",
-                    }}
-                  >
-                    {children}
-                  </ol>
-                ),
-
-                // Ex. 2: rendering custom lists
-                checkmarks: ({ children }) => (
-                  <ol
-                    style={{
-                      fontFamily: "'Istok Web', sans-serif",
-                      fontWeight: 700,
-                    }}
-                  >
-                    {children}
-                  </ol>
-                ),
-              },
-            }}
-          />
-
-          <Box
-            className="single-car-price-box"
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            margin="auto 1rem 1rem 1rem"
-            sx={{
-              boxShadow: 5,
-              padding: ".5rem 1rem",
-            }}
-          >
-            <Typography variant="h3">{carPrice}</Typography>
-            <Button variant="contained" sx={{ textTransform: "unset" }}>
-              <Typography variant="body1">{t("single-car.rent")}</Typography>
-            </Button>
-          </Box>
-        </Box>
-      </Grid2> */}
       <Box
         sx={{
           display: "flex",
@@ -228,6 +100,16 @@ const Car = () => {
             flexDirection="column"
             alignItems="center"
             gap="1rem"
+            sx={{
+              maxHeight: "500px",
+              overflowY: "auto",
+              paddingRight: "1rem",
+              "&::-webkit-scrollbar": {
+                display: "none",
+              },
+
+              scrollbarWidth: "none",
+            }}
           >
             <PortableText
               value={carData.description}
